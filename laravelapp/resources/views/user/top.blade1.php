4df95f9744e4c042dfe952e9962b372f7dc6a296
Nedashota <s.neda.sys21@morijyobi.ac.jp>
2023-08-23 14:20:04 +0900

    a
    a

---
+++ laravelapp/resources/views/user/top.blade1.php
@@ -0,0 +1,288 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <title>Morijyobi</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script src="https://cdn.firebase.com/js/client/2.3.2/firebase.js"></script>
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,900|Source+Serif+Pro&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="assets/css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="assets/fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="assets/css/aos.css">
+
+
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style2.scss">
+    
+
+   
+
+  </head>
+
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+    
+    <div class="site-wrap" id="home-section">
+
+      <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+          <div class="site-mobile-menu-close mt-3">
+            <span class="icon-close2 js-menu-toggle"></span>
+          </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+      </div>
+
+
+
+      <header class="site-navbar site-navbar-target" role="banner">
+
+        <div class="container">
+          <div class="row align-items-center position-relative">
+
+            <div class="col-3">
+              <div class="site-logo">
+                <a href="#I" class="font-weight-bold">morijyobi</a>
+              </div>
+            </div>
+
+            <div class="col-9  text-right">
+              
+
+              <span class="d-inline-block d-lg-none"><a class="text-primary site-menu-toggle js-menu-toggle py-5"></span></a></span>
+
+              
+
+              <nav class="site-navigation text-right ml-auto d-none d-lg-block" role="navigation">
+                <ul class="site-menu">
+                  <li class="active"><a class="nav-link" href="#P">01 state</a></li>
+                  <li><a class="nav-link" href="#D">02 overview</a></li>
+                  <li><a class="nav-link" href="#T">03 play</a></li>
+                  <li><a class="nav-link" href="#I">04 introduction</a></li>
+                </ul>
+              </nav>
+            </div>
+
+            
+          </div>
+        </div>
+
+      </header>
+
+
+    
+    <div class="site-section-cover">
+      <div class="container">
+        <div class="row align-items-center text-center justify-content-center">
+          <div class="col-lg-10">
+            <h1 class="text-white mb-4">Morijyobi</h1>
+            <p class="lead"></p>
+            <a id="P"></a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <section id="section03" class="demo">
+  <h1>Scroll Down Button</h1>
+  <a href="#section04"><span></span>Scroll</a>
+</section>
+
+    <div class="container-fluid portfolio-wrap bg-light pt-0 site-section">
+      <div class="row align-items-end mb-4">
+        <div class="col-md-3">
+          <a href="Bashpromotions/.vscode/Bash/bashpromotion.html" class="portfolio-item"><img src="assets/images/スクリーンショット (107).png" alt="Image" class="img-fluid"></a>
+        </div>
+        <div class="col-md-6">
+          <a href="Bashpromotions/.vscode/Bash/bashpromotion.html" class="portfolio-item"><img src="assets/images/スクリーンショット (108).png" alt="Image" class="img-fluid"></a>
+        </div>
+        <div class="col-md-3">
+          <a href="Bashpromotions/.vscode/Bash/bashpromotion.html" class="portfolio-item"><img src="assets/images/スクリーンショット (109).png" alt="Image" class="img-fluid"></a>
+        </div>
+      </div>
+      <div class="row align-items-start">
+        <div class="col-md-4">
+          <a href="web-site-main/Developer/drink.html" class="portfolio-item"><img src="assets/images/スクリーンショット 2023-08-22 145128.png" alt="Image" class="img-fluid"></a>
+        </div>
+        <div class="col-md-4">
+          <a href="web-site-main/Developer/index.html" class="portfolio-item"><img src="assets/images/スクリーンショット 2023-08-22 145316.png" alt="Image" class="img-fluid"></a>
+        </div>
+        <div class="col-md-4">
+          <a href="web-site-main/Developer/index.html" class="portfolio-item"><img src="assets/images/スクリーンショット 2023-08-22 145451.png" alt="Image" class="img-fluid"></a>
+        </div>
+      </div>
+    </div>
+    <a id="D"></a>
+    <div class="site-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="service-29191">
+              <span class="wrap-icon mb-4 d-block">
+                <span class="icon-leanpub"></span>
+              </span>
+              <h3 class="mb-3">デザイン系ゲーム</h3>
+              <p>プレイヤーは落下してくるオブジェクトを操作し、台座の上に
+                高く積み上げる。オブジェクトが台座から落下するとゲームオーバー。
+              </p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="service-29191">
+              <span class="wrap-icon mb-4 d-block">
+                <span class="icon-dribbble"></span>
+              </span>
+              <h3 class="mb-3">ビジネス系ゲーム</h3>
+              <p>プレイヤー（トラック）を操作し、ゴールまで荷物を届ける。
+                種類はプレイヤーを操作して障害物を避ける、見下ろし型２Ｄランゲーム。<br>
+              </p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="service-29191">
+              <span class="wrap-icon mb-4 d-block">
+                <span class="icon-local_bar"></span>
+              </span>
+              <h3 class="mb-3">システム系ゲーム</h3>
+              <p>高校生が対象の学校のＰＲを目的としたカジュアルゲーム。
+                マスコットである「ジョビジョビ君」をゴールに誘導する。<br>
+
+
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+    <a id="T"></a>
+    <div class="site-section bg-light">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3">
+            <img src="assets/images/samne1.png" alt="Image" class="img-fluid">
+          </div>
+          <div class="col-md-3">
+            <img src="assets/images/samne2.png" alt="Image" class="img-fluid">
+          </div>
+          <div class="col-md-3">
+            <img src="assets/images/samune3.png" alt="Image" class="img-fluid">
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+    
+    <a id="I"></a>
+    <div class="footer site-section bg-white">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3">
+            
+          </div>
+          <div class="d-flex vcard-91123 align-items-center">
+            <div class="img mr-3"><img src="assets/images/jyobikun.png" alt="Image" class="img-fluid"></div>
+            <div class="site-logo-footer">
+              <h3 class="mb-3">Morijyobi</h3>
+              <p>盛岡情報ビジネス&デザイン専門学校</p>
+            </div> 
+            <div class="name">
+              <span class="d-block">ああああ</span>
+              <span></span>
+            </div>
+          </div>
+          <div class="col-md-8 ml-auto">
+            <div class="row">
+             
+              <div class="col-md-4">
+                <ul class="list-unstyled links">
+                  <li><a href="#P">01 state</a></li>
+                  <li><a href="#D">02 overview</a></li>
+                  <li><a href="#T">03 play</a></li>
+                  <li><a href="#I">04 introduction</a></li>
+                </ul>
+              </div>
+             
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section">
+      <div class="container">
+        <div class="row mb-4 text-center">
+          <div class="col">
+            <a href="https://www.facebook.com/"><span class="icon-facebook"></span></a>
+            <a href="https://twitter.com/"><span class="icon-twitter"></span></a>
+            <a href="https://www.instagram.com/"><span class="icon-instagram"></span></a>
+     
+          </div>
+        </div>
+       
+      </div>
+    </div>
+
+    <a href="#" id="page-top">TOP</a>
+
+    
+
+    </div>
+    <script src="assets/js/jquery-3.3.1.min.js"></script>
+    <script src="assets/js/jquery-migrate-3.0.0.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/jquery.sticky.js"></script>
+    <script src="assets/js/jquery.waypoints.min.js"></script>
+    <script src="assets/js/jquery.animateNumber.min.js"></script>
+    <script src="assets/js/jquery.fancybox.min.js"></script>
+    <script src="assets/js/jquery.stellar.min.js"></script>
+    <script src="assets/js/jquery.easing.1.3.js"></script>
+    <script src="assets/js/bootstrap-datepicker.min.js"></script>
+    <script src="assets/js/isotope.pkgd.min.js"></script>
+    <script src="assets/js/aos.js"></script>
+  
+
+    <script src="assets/js/typed.js"></script>
+            <script>
+            var typed = new Typed('.typed-words', {
+            strings: ["Business"," Startups"," Organization", " Company"],
+            typeSpeed: 80,
+            backSpeed: 80,
+            backDelay: 4000,
+            startDelay: 1000,
+            loop: true,
+            showCursor: true
+            });
+            </script>
+
+
+    <script src="js/main.js"></script>
+    <script>
+      $(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
+</script>
+
+
+
+  </body>
+
+</html>
+
